33d5ab203a9e8e11abe74e7bd29df90b40c561d3
Sandip Gunale <sandipgunale257@gmail.com>
2026-06-25 13:48:18 +0530

    Project Setup

---
+++ skillforge-backend/src/test/java/com/project/skillforgebackend/SkillforgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.skillforgebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillforgeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
